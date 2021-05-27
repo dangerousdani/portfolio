0016ec4873c1308028b3f4a5a05fd39c3e61094f
DanielaVogel <MNNTAjtNpVtkEY>
2021-05-27 20:46:47 +0200

    added links

---
+++ links.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<?php include './includes/head.php';?>
+</head>
+
+<body>
+
+	<div style="display: flex; justify-content: center; align-items: center; margin-top: 50px">
+		<img src="images/_general/Logo-outline.svg" height="35px">
+	</div>
+	<div style="display: flex; justify-content: center; align-items: center; width: 100%">
+		<a style="font-size: 30px; font-weight: 300">@machtvogel</a>
+	</div>
+
+
+	<div class="animate-box" style="margin-top: 120px">
+
+		<div style="width: 100%; display: flex; justify-content: center; margin-bottom: 100px">
+			<a class="nav-points" style="font-size: 40px; margin-left: 0" target="_blank"
+				href="https://www.danielavogel.com">Portfolio</a>
+		</div>
+		<div style="width: 100%; display: flex; justify-content: center; margin-bottom: 100px">
+			<a class="nav-points" style="font-size: 40px; margin-left: 0" target="_blank"
+				href="https://www.behance.net/vogeldaniela">Behance</a>
+		</div>
+		<div style="width: 100%; display: flex; justify-content: center; margin-bottom: 100px">
+			<a class="nav-points" style="font-size: 40px; margin-left: 0" target="_blank"
+				href="https://www.linkedin.com/in/daniela-vogel/">LinkedIn</a>
+		</div>
+
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	</div>
+
+</body>
+
+</html>
